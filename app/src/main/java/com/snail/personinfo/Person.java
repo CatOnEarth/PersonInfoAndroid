6aa07cf152b03f170dd3329cb679f2653d6b7b09
CatOnEarth <maxt.space@gmail.com>
2022-08-21 18:24:12 +0300

    Firt commit

---
+++ app/src/main/java/com/snail/personinfo/Person.java
@@ -0,0 +1,138 @@
+package com.snail.personinfo;
+
+/** Class for save persons
+ *
+ */
+public class Person {
+    protected String email;
+    /**
+     * < user's email
+     */
+    protected String name;
+    /**
+     * < user's name
+     */
+    protected String surname;
+    /**
+     * < user's surname
+     */
+    protected int age;
+    /**
+     * < user's age
+     */
+    protected byte gender;   /**< user's gender*/
+
+    /**
+     * Default constructor of person
+     */
+    public Person() {
+
+    }
+
+    /**
+     * Constructor for class Person
+     *
+     * @param email   user's email
+     * @param name    user's name
+     * @param surname user's surname
+     * @param age     user's age
+     * @param gender  user's gender
+     */
+    public Person(String email, String name, String surname, int age, byte gender) {
+        this.email = email;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    /**
+     * Getter user's email
+     *
+     * @return user's email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Getter user's name
+     *
+     * @return user's name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Getter user's surname
+     *
+     * @return user's surname
+     */
+    public String getSurname() {
+        return surname;
+    }
+
+    /**
+     * Getter user's age
+     *
+     * @return user's age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * Getter user's gender
+     *
+     * @return user's gender
+     */
+    public byte getGender() {
+        return gender;
+    }
+
+    /**
+     * Setter user's email
+     *
+     * @param email user's email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * Setter user's name
+     *
+     * @param name user's name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Setter user's surname
+     *
+     * @param surname user's surname
+     */
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    /**
+     * Setter user's age
+     *
+     * @param age user's age
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * Setter user's gender
+     *
+     * @param gender user's gender
+     */
+    public void setGender(byte gender) {
+        this.gender = gender;
+    }
+}
